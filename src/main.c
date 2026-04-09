13134c5f910c28d0d2639d57ee48a6bbded72a44
Mateus <garciamateus285@gmail.com>
2026-04-09 20:58:51 -0300

    fix: corrigir erro de main declarado em dois arquivos .c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include "types.h"
+#include "algoritmo_genetico.h"
 
-int main(){
+#include "leitura.c"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s <arquivo.knap>\n", argv[0]);
+        return 1;
+    }
+
+    Mochila m = ler_arquivo(argv[1]);
+    exibir_mochila(&m);
+    liberar_mochila(&m);
+
     return 0;
 }

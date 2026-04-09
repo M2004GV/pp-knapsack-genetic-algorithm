c44e47f01841b224e1d04e8d25a75928667a8c91
Mateus <garciamateus285@gmail.com>
2026-04-09 20:53:58 -0300

    fix: corregir nomes de variáveis erradas

--- include/algoritmo_genetico.h
+++ include/algoritmo_genetico.h
@@ -3,13 +3,13 @@
 
 #include "types.h"
 
-Populacao criar_populacao(int tamanho, int n_items);
+Populacao criar_populacao(int tamanho, int n_itens);
 void destruir_populacao(Populacao *populacao);
 void inicializar_populacao(Populacao *populacao);
-void avaliar_populacao(Populacao *populacao, InstanciaMochila *instancia);
+void avaliar_populacao(Populacao *populacao, Mochila *instancia);
 
 int torneio(Populacao *populacao, int k);
 
-void crossover(Individuo *pai1, Individuo *pai2, Individuo *filho1, Individuo *filho2, int n_items);
+void crossover(Individuo *pai1, Individuo *pai2, Individuo *filho1, Individuo *filho2, int n_itens);
 
 #endif // GA_H

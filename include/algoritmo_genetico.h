d89feff015be9ba225ee9caeb0825b97278b83f2
Mateus <garciamateus285@gmail.com>
2026-04-09 22:05:03 -0300

    teste

--- include/algoritmo_genetico.h
+++ include/algoritmo_genetico.h
@@ -5,13 +5,16 @@
 
 Populacao criar_populacao(int tamanho, int n_itens);
 void destruir_populacao(Populacao *populacao);
-void inicializar_populacao(Populacao *populacao);
+void inicializar_populacao(Populacao *populacao, int n_itens);
 void avaliar_populacao(Populacao *populacao, Mochila *instancia);
 
 int torneio(Populacao *populacao, int k);
 
 void crossover(Individuo *pai1, Individuo *pai2, Individuo *filho1, Individuo *filho2, int n_itens);
 void mutacao(Individuo *individuo, double taxa_mutacao, int n_itens);
+
+Individuo melhor_individuo(Populacao *populacao, int n_itens);
+void executar_algoritmo_genetico(Mochila *instancia, int tamanho_populacao, int n_geracoes, double taxa_mutacao, int k_torneio);
 
 int inline rand_int(int min, int max) {   return min + rand() % (max - min + 1); }
 
--- src/algoritmo_genetico.c
+++ src/algoritmo_genetico.c
@@ -43,17 +43,15 @@ void destruir_populacao(Populacao *populacao) {
     populacao->tamanho = 0;
 }
 
-void inicializar_populacao(Populacao *populacao){
+void inicializar_populacao(Populacao *populacao, int n_itens){
     for(int i = 0; i < populacao->tamanho; i++){
-        for(int j = 0; j < populacao->individuos[i].genes[j]; j++){
-            populacao->individuos[i].genes[j] = rand_int(0, 1);
+        for(int j = 0; j < n_itens; j++){
+            populacao->individuos[i].genes[j] = rand() % 2;
         }
     }
 }
 
 void avaliar_populacao(Populacao *populacao, Mochila *instancia){
-
-    
     for(int i = 0; i < populacao->tamanho; i++){
         int peso_total = 0;
         int valor_total = 0;
@@ -78,19 +76,16 @@ void avaliar_populacao(Populacao *populacao, Mochila *instancia){
 }
 
 int torneio(Populacao *populacao, int k){
-
     int melhor_idx = rand_int(0, populacao->tamanho - 1);
 
     for( int i = 1; i < k; i++){
         int comp =  rand_int(0, populacao->tamanho - 1);
 
-        if(populacao->individuos[comp].fitness > populacao->individuos[melhor_idx].fitness) melhor_idx = comp;
-        
+        if(populacao->individuos[comp].fitness > populacao->individuos[melhor_idx].fitness) melhor_idx = comp;   
     }
     return melhor_idx;
 }
 void crossover(Individuo *pai1, Individuo *pai2, Individuo *filho1, Individuo *filho2, int n_itens){
-
     int ponto_corte = rand_int(1, n_itens - 1);
 
     for(int i = 0; i < n_itens; i++){
@@ -111,4 +106,102 @@ void mutacao(Individuo *individuo, double taxa_mutacao, int n_itens){
         }
     }
 
+}
+
+Individuo melhor_individuo(Populacao *populacao, int n_itens){
+    int melhor = 0;
+    for(int i = 1; i < populacao->tamanho; i++){
+        if(populacao->individuos[i].fitness > populacao->individuos[melhor].fitness){
+            melhor = i;
+        }
+    }
+
+    Individuo copia;
+    copia.fitness = populacao->individuos[melhor].fitness;
+    copia.peso_total = populacao->individuos[melhor].peso_total;
+    copia.valor_total = populacao->individuos[melhor].valor_total;
+    copia.genes = (int *)malloc(n_itens * sizeof(int));
+    memcpy(copia.genes, populacao->individuos[melhor].genes, n_itens * sizeof(int));
+
+    return copia;
+}
+
+void executar_algoritmo_genetico(Mochila *instancia, int tamanho_populacao, int n_geracoes, double taxa_mutacao, int k_torneio){
+    int n = instancia->n_itens;
+
+    // garante tamanho par para o crossover trabalhar em pares
+    if (tamanho_populacao % 2 != 0) {
+        tamanho_populacao++;
+    }
+
+    // cria, inicializa e avalia a população inicial
+    Populacao pop = criar_populacao(tamanho_populacao, n);
+    inicializar_populacao(&pop, n);
+    avaliar_populacao(&pop, instancia);
+
+    // guarda o melhor encontrado ao longo de todas as gerações
+    Individuo melhor_geral = melhor_individuo(&pop, n);
+
+    printf("=== Algoritmo Genetico ===\n");
+    printf("Populacao : %d | Geracoes : %d | Torneio k=%d | Mutacao : %.1f%%\n\n",
+           tamanho_populacao, n_geracoes, k_torneio, taxa_mutacao * 100.0);
+
+    for (int g = 0; g < n_geracoes; g++) {
+
+        Populacao nova_pop = criar_populacao(tamanho_populacao, n);
+
+        // preenche nova geração par a par
+        for (int i = 0; i < tamanho_populacao; i += 2) {
+            int idx1 = torneio(&pop, k_torneio);
+            int idx2 = torneio(&pop, k_torneio);
+
+            Individuo *pai1   = &pop.individuos[idx1];
+            Individuo *pai2   = &pop.individuos[idx2];
+            Individuo *filho1 = &nova_pop.individuos[i];
+            Individuo *filho2 = &nova_pop.individuos[i + 1];
+
+            crossover(pai1, pai2, filho1, filho2, n);
+            mutar(filho1, n, taxa_mutacao);
+            mutar(filho2, n, taxa_mutacao);
+        }
+
+        avaliar_populacao(&nova_pop, instancia);
+
+        // atualiza melhor geral se a geração produziu solução superior
+        Individuo melhor_geracao = melhor_individuo(&nova_pop, n);
+        if (melhor_geracao.fitness > melhor_geral.fitness) {
+            free(melhor_geral.genes);
+            melhor_geral = melhor_geracao;
+        } else {
+            free(melhor_geracao.genes);
+        }
+
+        // substitui população antiga pela nova
+        destruir_populacao(&pop);
+        pop = nova_pop;
+
+        // exibe progresso a cada 10 gerações e na primeira
+        if (g == 0 || (g + 1) % 10 == 0) {
+            printf("Geracao %4d | Melhor fitness: %d | Peso: %d / %d\n",
+                   g + 1,
+                   melhor_geral.fitness,
+                   melhor_geral.peso_total,
+                   instancia->capacidade);
+        }
+    }
+
+    // exibe resultado final
+    printf("\n=== Resultado Final ===\n");
+    printf("Melhor valor encontrado : %d\n", melhor_geral.fitness);
+    printf("Peso total utilizado    : %d / %d\n", melhor_geral.peso_total, instancia->capacidade);
+    printf("Itens selecionados      : ");
+    for (int i = 0; i < n; i++) {
+        if (melhor_geral.genes[i]) {
+            printf("%d ", i + 1);
+        }
+    }
+    printf("\n");
+
+    free(melhor_geral.genes);
+    destruir_populacao(&pop);
 }
